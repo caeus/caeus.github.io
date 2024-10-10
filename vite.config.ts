d0906463d46a64dd0df9c7001f0785a0e58e3fd5
Alejandro Navas <caeus@Alejandros-MacBook-Pro.local>
2024-10-10 16:37:10 +0200

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  build:{
+    outDir:'docs',
+    emptyOutDir:true
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

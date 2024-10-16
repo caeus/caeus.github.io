f82e2cf7fb5fab979e4f95a948451dff75cc7aeb
Alejandro Navas <caeus@Alejandros-MacBook-Pro.local>
2024-10-16 14:48:56 +0200

    ajshd

--- src/devto/devto-client.ts
+++ src/devto/devto-client.ts
@@ -26,6 +26,9 @@ export class DefaultDevtoClient implements DevtoClient {
     if (page != undefined) {
       params.page = String(page)
     }
+    const headers = new Headers()
+    headers.append('Pragma','no-cache')
+    headers.append('Cache-Control','no-cache')
     return fetch(`${this.config.base_url}/articles?${new URLSearchParams(params)}`).then((r) => {
       if (r.ok) return r.json()
       return r.json().then((r) => Promise.reject(r))
